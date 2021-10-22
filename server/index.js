79103e0ca06ad64d5981f3e5531078332eea8dd3
Batuhan Ozturk <fbatuhanr@gmail.com>
2021-10-22 23:56:31 +0300

    nodejs module & mongodb (mongoose) installation

--- server/index.js
+++ server/index.js
@@ -1,31 +1,43 @@
 const express = require("express");
-const bodyParser = require('body-parser');
+const cors = require("cors");
+const corsOptions = {
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
+const mongoose = require('mongoose');
+
 
 const PORT = process.env.PORT || 3001;
 
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 
-app.get("/message", (req, res) => {
-  res.json({ message: "Hello from server!" });
-});
+const dbURI = "mongodb+srv://admin:jmnZTIrVMA05hPYj@cluster0.w4wrz.mongodb.net/profile-management?retryWrites=true&w=majority";
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then((result) => console.log(result, "connected to dbd"))
+        .catch((error) => console.log("HATA!:",error));
 
-app.get("/items", (req, res) => {
-  res.json({
-    books: [
-      "kitap1",
-      "kitap2"
-    ]
-  });
+
+app.get("/users", (req, res) => {
+
+    res.json({user: "ben user"});
 });
 
 app.post("/login", function (req, res) {
   var userName = req.body.user;
   var password = req.body.password;
-  if(userName == 'Batu' && password == '1234'){
-    return res.json({isSuccess: true, errorMessage: null});
+  if(userName == "fbatuhanr@gmail.com" && password == "123"){
+
+    return res.json({
+      isLoginSuccess: true, 
+      errorMessage: null
+    });
   }
-  return res.json({isSuccess: false, errorMessage: 'Kullanıcı bilgilerini kontrol ediniz.'});
+  return res.json({isLoginSuccess: false, errorMessage: 'Kullanıcı bilgilerini kontrol ediniz.'});
 });
 
 app.listen(PORT, () => {
