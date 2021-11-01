dd9cda7f6bc5b484409ba6b4d4590d0db11b87fb
Batuhan Ozturk <fbatuhanr@gmail.com>
2021-11-01 16:24:32 +0300

    Update userProfile.js

--- Schemas/userProfile.js
+++ Schemas/userProfile.js
@@ -5,7 +5,18 @@ const Schema = mongoose.Schema;
 // const { Schema } = mongoose;
 
 const userProfileSchema = new Schema({
-  email:  String 
+
+  email: {type: String},
+  password: {type: String},
+
+  name: {type: String},
+  surname: {type: String},
+  phoneNumber: {type: String},
+  education: {type: String},
+  country: {type: String},
+  state: {type: String},
+  hobbies: {type: []}
+
 }, {timestamps: true});
 
 const userProfile = mongoose.model('userProfile', userProfileSchema);
